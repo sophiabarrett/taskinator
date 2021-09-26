eed3d1ab0ce7430b5ab444c45ef0e67d66a75d83
Sophia Barrett <sophia@yoursummit.media>
2021-09-26 14:30:30 -0700

    add ability to create new task

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+var buttonEl = document.querySelector("#save-task");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+var createTaskHandler = function() {
+    var taskItemEl = document.createElement("li");
+    taskItemEl.textContent = "New task";
+    taskItemEl.className = "task-item";
+    tasksToDoEl.appendChild(taskItemEl);
+}
+
+buttonEl.addEventListener("click", createTaskHandler);
