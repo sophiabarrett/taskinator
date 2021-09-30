ba6b90b657dec3c82ec241f81112010e133bb1ac
Sophia Barrett <sophia@yoursummit.media>
2021-09-30 12:24:42 -0700

    add delete button functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+var pageContentEl = document.querySelector("#page-content");
 var taskIdCounter = 0;
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
@@ -98,4 +99,18 @@ var createTaskActions = function (taskId) {
     return actionContainerEl;
 };
 
+var taskButtonHandler = function(event) {
+    if (event.target.matches(".delete-btn")) {
+        var taskId = event.target.getAttribute("data-task-id");
+        deleteTask(taskId);
+    }
+};
+
+var deleteTask = function(taskId) {
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    taskSelected.remove();
+};
+
 formEl.addEventListener("submit", taskFormHandler);
+
+pageContentEl.addEventListener("click", taskButtonHandler);
